e5829110dac735cd40aa86e4c931d38dcf3abb3b
Divyanshu Singh <dsinecos@gmail.com>
2017-11-28 12:23:50 +0530

    Display the server PORT on console

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ var app = require('./app.js');
 
 var PORT = process.env.PORT || 2348;
 
-app.listen(PORT);
+app.listen(PORT, notifyServerStartedOnConsole(PORT));
+
+function notifyServerStartedOnConsole(PORT) {
+    console.log("Server started on PORT : " + PORT);
+}
