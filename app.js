49d88055c4a77602584ae056cea8344a7c41e4d3
Divyanshu Singh <dsinecos@gmail.com>
2018-01-05 12:56:19 +0530

    Acceps cross-domain requests with credentials (cookies)

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ var snippet = require('./api/snippet/snippetRoutes.js');
 var app = express();
 
 var allowCrossDomain = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Headers', 'Content-Type');
