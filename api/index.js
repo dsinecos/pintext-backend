ea84ef18d6b197ee66690f5b3eb772622334c282
Divyanshu Singh <dsinecos@gmail.com>
2018-01-08 15:19:19 +0530

    Modify base route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    res.status(200).json({
+        data: 'API Active. Request received'
+    })
   
 });
 
