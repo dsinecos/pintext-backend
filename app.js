9c07178b14f4a921330edd12813c26eac115e3db
Divyanshu Singh <dsinecos@gmail.com>
2018-01-10 14:52:41 +0530

    Enable browser only sessions and allow client side code to access cookie
    'cookie.maxAge: null'
    'cookie.httpOnly: false'

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ var sessionOptions = {
   resave: false,
   saveUninitialized: false,
   store: PostgreSqlStore,
-  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
+  // cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
+  cookie: { maxAge: null, httpOnly: false }
 };
 
 var index = require('./api/index');
